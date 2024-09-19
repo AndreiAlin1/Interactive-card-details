2c3f0d8f1c70a392fe2eabeedf7c65eb6286963c
AndreiAlin1 <alinandrei0303@gmail.com>
2024-09-19 17:11:28 +0300

    Commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,26 @@ function toggleConfirmationMessage() {
     const confirmationMessage = document.querySelector('.js-confirmation-message');
 
     rightContainer.style.display = 'none';
+    confirmationMessage.style.display='block';
 }
+
+const inputHolder = document.querySelector('.js-input-holder');
+const cardFrontText = document.querySelector('.js-card-name-text');
+
+inputHolder.addEventListener('input', function(){
+    cardFrontText.textContent = inputHolder.value;
+})
+
+const inputName = document.querySelector('.js-input-card');
+const cardNameText = document.querySelector('.js-card-front-text');
+
+inputName.addEventListener('input',function(){
+    cardNameText.textContent = inputName.value;
+})
+
+const inputCvC = document.querySelector('.js-CVC-input');
+const cvcText = document.querySelector('.js-card-cvv-text');
+
+inputCvC.addEventListener('input',function(){
+    cvcText.textContent = inputCvC.value;
+})
