ebacbd84fba46d162e5b84e61204e58a066f0514
AndreiAlin1 <alinandrei0303@gmail.com>
2024-09-19 16:25:16 +0300

    Commit

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+function toggleConfirmationMessage() {
+    const rightContainer = document.querySelector('.js-right-container');
+    const confirmationMessage = document.querySelector('.js-confirmation-message');
+
+    rightContainer.style.display = 'none';
+}
